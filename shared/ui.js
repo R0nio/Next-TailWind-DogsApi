a8f338294fc07c8b3d28d9c42c491052299c3d2e
R0nio <dimabirucob@gmail.com>
2025-10-02 09:19:58 +0500

    Добавление кнопки и использование Tailwind

--- shared/ui.js
+++ shared/ui.js
@@ -3,10 +3,10 @@
 
 export const Button = ({children}) =>{
     return(
-        <div className="bg-[#4d86ff]">
+        <div className="bg-[#4d86ff] px-[20px] py-1 w-fit rounded-full transition cursor-pointer">
             <a href="#">
                 <button>
-                    <p className="text-white">{children}</p>
+                    <p className="text-white text-sm">{children}</p>
                 </button>
             </a>
         </div>
